335593fc78c7ffe7014bd6ad9b57b2d031c10cb1
Meghaj MB <meghajmb2001@gmail.com>
2024-06-20 20:57:28 +0530

    Version 5.3.2

--- controllers/user.js
+++ controllers/user.js
@@ -30,6 +30,10 @@ var instance = new Razorpay({
 
 const transporter = nodemailer.createTransport({
     service:"gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
+    ignoreTLS: false,
+    secure: false,
     auth: {
         user: process.env.USER,
         pass: process.env.USER_PASSWORD,
